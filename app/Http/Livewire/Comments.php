4d976bcd2e2b56f2efef9beae9ba703af6ca7d6e
yohan72565 <yohanchathuranga72565@gmail.com>
2020-07-22 18:49:06 -0700

    livewire flash messages

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -31,6 +31,8 @@ class Comments extends Component
         $createdComment = Comment::create(['body'=> $this->newComment,'user_id'=>1]);
         $this->comments->prepend($createdComment);
         $this->newComment="";
+
+        session()->flash('message', 'Comment added successfully..!');
     }
 
     public function remove($commentId){
@@ -38,6 +40,7 @@ class Comments extends Component
         $comment->delete();
         $this->comments = $this->comments->except($commentId);
         // dd($comment);
+        session()->flash('messagedel', 'Comment deleted successfully..!');
     }
         
 
