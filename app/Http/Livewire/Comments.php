bc22ac0a39927b7f279131439cd2c57fb2cc01f1
yohan72565 <yohanchathuranga72565@gmail.com>
2020-07-28 22:11:16 -0700

    Store image using intervention

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -10,6 +10,9 @@ use App\Comment;
 
 use Livewire\WithPagination;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic;
+use Illuminate\Support\Str;
 
 class Comments extends Component
 {
@@ -34,19 +37,33 @@ class Comments extends Component
     // }
 
     public function addComment(){
-
         $this->validate(['newComment'=>'required|max:255']);
+        $image = $this->storeImage();
 
-        $createdComment = Comment::create(['body'=> $this->newComment,'user_id'=>1]);
+        $createdComment = Comment::create(['body'=> $this->newComment,'user_id'=>1,'image'=> $image]);
         // $this->comments->prepend($createdComment);
         $this->newComment="";
 
         session()->flash('message', 'Comment added successfully..!');
     }
 
+    public function storeImage(){
+        if(!$this->image){
+            return null;
+        }
+        
+        $img = ImagemanagerStatic::make($this->image)->encode('jpg');
+        $name = Str::random().'.jpg';
+        Storage::disk('public')->put($name,$img);
+        return $name;
+    }
+
     public function remove($commentId){
+        
         $comment = Comment::find($commentId);
+        Storage::disk('public')->delete($comment->image);
         $comment->delete();
+        
         // $this->comments = $this->comments->except($commentId);
         // dd($comment);
         session()->flash('messagedel', 'Comment deleted successfully..!');
--- database/migrations/2020_07_22_062538_create_comments_table.php
+++ database/migrations/2020_07_22_062538_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('body');
+            $table->string('image')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
